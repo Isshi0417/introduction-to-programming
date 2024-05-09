ab4fbf9584dad2a7e75447435a56431f8958e4a1
Sho Ishida <isshi0417@gmail.com>
2024-05-09 23:18:37 +0900

    Upload exercises for Data Types, Basic Operations, Variables, Input/Output, Functions and Methods, and Flow Control

---
+++ data_types.py
@@ -0,0 +1,32 @@
+# 1. Identify the data type or class for each of the following values.
+'''
+String
+Boolean
+Tuple
+Float
+List
+Integer
+Range
+Set
+NoneType
+Dictionary
+'''
+
+# 2. Create a tuple called 'names' that contain several pet names.
+names = (
+    'Asta',
+    'Butterscotch',
+    'Pudding', 
+    'Neptune', 
+    'Darwin',
+)
+
+# 3. Create a dictionary named pets that contains a list of pet names and the
+# type of animal.
+pets = {
+    'Asta' :         'dog',
+    'Butterscotch' : 'cat',
+    'Pudding' :      'cat',
+    'Neptune' :      'fish',
+    'Darwin' :       'lizard',
+}
---
+++ inputs_and_outputs.py
@@ -0,0 +1,17 @@
+# 1. Write a program named 'greeter.py'. The program should ask for your name,
+# then output 'Hello, NAME!' where NAME is the name you entered.
+'''
+File in /inputs_and_outputs_files.
+'''
+
+# 2. Modify the greeter.py program to ask for the user's first and last names
+# separately, then greet the user with their full name.
+'''
+File in /inputs_and_outputs_files.
+'''
+
+# 3. Write a program named 'age.py' that asks the user to enter their age,
+# then calculates and reports the future age 10, 20, 30, and 40 years from now.
+'''
+File in /inputs_and_outputs_files.
+'''
