c9aabaad841df982e377ec5ca61441437e0ee4ce
Sho Ishida <isshi0417@gmail.com>
2024-05-10 20:17:46 +0900

    Removed input() for age

--- variables_files/age.py
+++ variables_files/age.py
@@ -1,5 +1,6 @@
 # 6. Write a program named 'age.py' that includes someone's age and then 
 # calculates and reports the future age 10, 20, 30, and 40 years from now.
+
 age = 22
 print(f'In 10 years, you will be {age + 10} years old')
 print(f'In 20 years, you will be {age + 20} years old')
